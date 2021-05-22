a29614b7150f08c51dfcb6eff59899c9ab4cc7fe
Carlos Aparicio <carlos_non86@msn.com>
2021-05-22 17:03:50 +0200

    create fake models

---
+++ app/models/specialistic-terms.js
@@ -0,0 +1,26 @@
+const specialisticTerms = [
+    {
+        id: 0,
+        source: "US Dollars",
+        target: "Dollari USA",
+        active: false,
+    },
+    {
+        id: 1,
+        source: "Britannic Proctectorship",
+        target: "Protettorato Britannico",
+        active: false,
+    },
+    {
+        id: 2,
+        source: "Gross domestic product",
+        target: "Prodotto interno lordo",
+        active: false,
+    },
+    {
+        id: 3,
+        source: "Britannic Proctectorship",
+        target: "Protettorato Britannico",
+        active: false,
+    },
+];
